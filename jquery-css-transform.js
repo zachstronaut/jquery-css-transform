cb06ce14bae5be4a872d817058ad8ad18cef7175
Zachary Johnson <me@zachstronaut.com>
2009-05-24 01:06:08 -0500

    Updated to usual jQuery patch/plugin pattern where jQuery is passed in as $ argument. Monkey Patched call to css() really only needed key as a named argument. Neither of these updates really change anything.  Mostly just code cleanliness issues.

--- jquery-css-transform.js
+++ jquery-css-transform.js
@@ -1,4 +1,4 @@
-(function() {
+(function ($) {
     // Override default jQuery css() method to support CSS 'transform' property
     // uniformly across Webkit/Safari and Firefox 3.1+
     // 2009 Zachary Johnson www.zachstronaut.com
@@ -15,19 +15,19 @@
         return 'transform';
     }
     
-    var proxied = jQuery.fn.css;
-    jQuery.fn.css = function(key, value) {
+    var proxied = $.fn.css;
+    $.fn.css = function(key) {
         if (key == 'transform') {
-            if (typeof jQuery.props[key] == 'undefined') {
-                jQuery.props[key] = getTransformProperty(this.get(0));
+            if (typeof $.props[key] == 'undefined') {
+                $.props[key] = getTransformProperty(this.get(0));
             }
             // We force the property mapping here because jQuery.attr() does
             // property mapping with jQuery.props when setting a CSS property,
             // but curCSS() does *not* do property mapping when *getting* a
             // CSS property.  (It probably should since it manually does it
             // for 'float' now anyway.)
-            key = jQuery.props[key];
+            key = $.props[key];
         }
         return proxied.apply(this, arguments);
     };
-})();
+})(jQuery);
