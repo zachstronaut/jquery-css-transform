bca58da823954401a1586716de7cf80b56a45164
Zachary Johnson <me@zachstronaut.com>
2009-07-15 21:49:55 -0500

    OH HAI Changes made to make sure that css('transform', value) and css({transform: value}) both work

--- jquery-css-transform.js
+++ jquery-css-transform.js
@@ -1,33 +1,61 @@
 (function ($) {
     // Override default jQuery css() method to support CSS 'transform' property
-    // uniformly across Webkit/Safari and Firefox 3.1+
+    // uniformly across Webkit/Safari/Chrome and Firefox 3.5+
     // 2009 Zachary Johnson www.zachstronaut.com
-    function getTransformProperty(element) {
+    function getTransformProperty(element)
+    {
         // Try transform first for forward compatibility
         var properties = ['transform', 'WebkitTransform', 'MozTransform'];
         var p;
-        while (p = properties.shift()) {
-            if (typeof element.style[p] != 'undefined') {
+        while (p = properties.shift())
+        {
+            if (typeof element.style[p] != 'undefined')
+            {
                 return p;
             }
         }
+        
         // Default to transform also
         return 'transform';
     }
     
     var proxied = $.fn.css;
-    $.fn.css = function(key) {
-        if (key == 'transform') {
-            if (typeof $.props[key] == 'undefined') {
-                $.props[key] = getTransformProperty(this.get(0));
-            }
-            // We force the property mapping here because jQuery.attr() does
-            // property mapping with jQuery.props when setting a CSS property,
-            // but curCSS() does *not* do property mapping when *getting* a
-            // CSS property.  (It probably should since it manually does it
-            // for 'float' now anyway.)
-            key = $.props[key];
+    $.fn.css = function (arg)
+    {
+        // Find the correct browser specific property and setup the mapping using
+        // $.props which is used internally by jQuery.attr() when setting CSS
+        // properties via either the css(name, value) or css(properties) method.
+        // The problem with doing this once outside of css() method is that you
+        // need a DOM node to find the right CSS property, and there is some risk
+        // that somebody would call the css() method before body has loaded or any
+        // DOM-is-ready events have fired.
+        if
+        (
+            typeof $.props['transform'] == 'undefined'
+            &&
+            (
+                arg == 'transform'
+                ||
+                (
+                    typeof arg == 'object'
+                    && typeof arg['transform'] != 'undefined'
+                )
+            )
+        )
+        {
+            $.props['transform'] = getTransformProperty(this.get(0));
         }
+        
+        // We force the property mapping here because jQuery.attr() does
+        // property mapping with jQuery.props when setting a CSS property,
+        // but curCSS() does *not* do property mapping when *getting* a
+        // CSS property.  (It probably should since it manually does it
+        // for 'float' now anyway... but that'd require more testing.)
+        if (arg == 'transform')
+        {
+            arg = $.props['transform'];
+        }
+        
         return proxied.apply(this, arguments);
     };
 })(jQuery);
